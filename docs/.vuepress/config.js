5d18365cac78a2fd55b53f373470926b93753ac7
huangjingpeng <395530814@qq.com>
2022-09-06 20:43:36 +0800

    feat: 添加文档标题和头部导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  title: 'UI-DEMO',
   base: '/ui-demo/',
   themeConfig: {
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: 'Github', link: 'https://github.com/sowhat31/ui-demo'},
+      {text: 'Gitee', link: 'https://gitee.com/huang_jing_peng/mooc-ui'},
+      {text: 'Npm', link: 'https://www.npmjs.com/package/ui-demo-sw31'}
+    ],
     sidebar: [
       '/',
       '/componentDocs/card'
